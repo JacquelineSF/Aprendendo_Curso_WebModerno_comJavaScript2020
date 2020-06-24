b5e10cf393429065165b09831bd4d28cf5dc2181
Jacqueline dos Santos <jacquelinevps@hotmail.com>
2020-06-24 11:52:45 -0300

    #

---
+++ FUndamentos/organização.js
@@ -0,0 +1,6 @@
+console.log('Sentença de código')
+ console.log(1+2)
+
+{
+
+}
---
+++ FUndamentos/variaveieconstantes.js
@@ -0,0 +1,11 @@
+var a = 3    //vari a recebe 3
+let b = 5
+ 
+a=30  //tbm pode declarar a variavel dnovo, se for de tipo var. var = 30.
+b=60
+
+console.log(a,b)
+
+const c = 5  // não tem como mudar, ele não varia. Entao se no seu codigo algo nao tem a necessidade de mudar, use const
+
+console.log(c)
